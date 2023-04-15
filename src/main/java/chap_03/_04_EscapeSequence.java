939b4aef97ba94dfc19a1a703a86a646c1d8af26
aoqlsdl <bini92444@gmail.com>
2023-04-15 22:52:14 +0900

    ✨feat: chap_03_04 특수 문자

---
+++ src/main/java/chap_03/_04_EscapeSequence.java
@@ -0,0 +1,30 @@
+package chap_03;
+
+public class _04_EscapeSequence {
+    public static void main(String[] args) {
+        // 특수 문자, 이스케이프 문자 (Escape Sequence, Escape Character, Special Character)
+        // \n, \t, \\, \", \'
+        System.out.println("자바가");
+        System.out.println("너무");
+        System.out.println("재미?있어요?");
+
+        System.out.println("자바가\n너무\n재미?있어요?"); // \n: 줄바꿈
+
+        // 해물파전 9000원
+        // 김치전  8000원
+        // 부추전  8000원
+        System.out.println("해물파전\t\t9000원");
+        System.out.println("김치전\t\t8000원");
+        System.out.println("부추전\t\t8000원"); // \t: tap
+
+        System.out.println("C:\\Program Files\\Java"); // \\ : 역슬래시
+
+        System.out.println("단비가 \"나옹\"이라고 했어요."); // \" : 큰 따옴표 -> 단비가 "냐옹"이라고 했어요.
+        System.out.println("단비가 \'뭘 봐?\'라는 표정을 지었어요."); // \' : 작은 따옴표 -> 단비가 '뭘 봐?'라는 표정을 지었어요.
+        System.out.println("단비가 '뭘 봐?'라는 표정을 지었어요.");
+
+        char c = 'A';
+        c = '\''; // char 변수에 '를 넣고 싶을 때 사용
+        System.out.println(c);
+    }
+}

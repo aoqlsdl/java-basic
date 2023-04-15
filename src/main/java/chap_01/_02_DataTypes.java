cd6e2d9fc4727e744e469351286bf8a3336be26e
aoqlsdl <bini92444@gmail.com>
2023-04-15 19:05:22 +0900

    ✨feat: chap_02 자료형

---
+++ src/main/java/chap_01/_02_DataTypes.java
@@ -0,0 +1,24 @@
+package chap_01;
+
+public class _02_DataTypes {
+    // psvm을 입력하면 main 영역을 자동으로 만들어줌
+    public static void main(String[] args) {
+        // 단축어: sout -> System.out.println();
+
+        // 문자열 String
+        System.out.println("hello, world!");
+        System.out.println("안녕하세요?");
+
+        // 숫자 int
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.141592);
+
+        // boolean
+        System.out.println(true);
+        System.out.println(false);
+
+        // 계산식도 입력 가능
+        System.out.println(123 + 345);
+    }
+}
